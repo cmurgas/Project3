2c3c50163db691b8029daca7a430554614040163
CosmicLamb <31945143+CosmicLamb@users.noreply.github.com>
2017-11-30 12:30:04 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
-package com.jetbbrains;
+package project3;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-	    System.out.println("'Hello Git Hub' -Katie");
-	    System.out.println("Hello Git Hub - Corey");
-	    System.out.println("Hello Git Hub - Corey");
-    }
+public class Main extends Application {
+
+   @Override
+   public void start(Stage primaryStage) throws Exception{
+       Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+       primaryStage.setTitle("Hello World");
+       primaryStage.setScene(new Scene(root, 300, 275));
+       primaryStage.show();
+   }
+
+
+   public static void main(String[] args) {
+       launch(args);
+   }
 }
+
