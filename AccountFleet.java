abefa2a7f2f11f5bb66d261f77c28b0b23b479a9
CosmicLamb <31945143+CosmicLamb@users.noreply.github.com>
2017-12-03 18:34:19 -0500

    Update AccountFleet.java

--- AccountFleet.java
+++ AccountFleet.java
@@ -1,115 +1,95 @@
 package project3;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
 /**
  *
- * @author Karen Cassine
+ * @author Karen Cassine, Katie Lamb
  */
 public class AccountFleet {
-    
-    ArrayList <Account> accountFleet = new ArrayList<>();
-    
-//************************************
-//PROPOSED CHANGES TO ACCOUNT FLEET IN ORDER TO CREATE A FILE:
 
-file = new File("accountFleet.txt");
-try{
-   if (file.createNewFile()) {
-       System.out.println("account fleet created.");
-       accountFleet = new ArrayList<>();
-   }
-}
-catch (IOException e){
-   System.out.println("account fleet already exists.");
-}
-//THIS WOULD REQUIRE A File file; DECLARATION AT THE BEGINNING
-//***************************************
+
+    ArrayList <Account> accountFleet;
+    File file = new File("accounts");
 
 
     public AccountFleet(){
-    this.accountFleet = accountFleet; 
-    
-    } 
+        file = new File("accountFleet.txt");
+        try{
+            if (file.createNewFile()) {
+                System.out.println("account fleet created.");
+                accountFleet = new ArrayList<>();
+            }
+        }
+        catch (IOException e){
+            System.out.println("account fleet already exists.");
+        }
 
-
-//****************************************
-//PROPOSED WRITE/SAVE METHOD:
-public void saveAccountChanges(){
-   if(accountFleet.size() > 0){
-       try {
-           FileWriter fileWriter = new FileWriter(file);
-           BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-           for(Account acc : accountFleet){
-               bufferedWriter.write(acc.getFirstName()+","+
-                       acc.getLastName()+","+
-                       acc.getUserName()+","+
-                       acc.getPassword()+","+
-                       acc.getEmail()+","+
-                       acc.getPhoneNumber());
-               bufferedWriter.newLine();
-           }
-           bufferedWriter.close();
-       }catch(IOException ex){ ex.printStackTrace(); }
-   }
-}
-//WOULD BE CALLED AT THE END OF EACH DO-METHOD IN THE CONTROLLERS THAT //CHANGES SOMETHING ABOUT ACCOUNT FLEET
-//WOULD WRITE CHANGES TO PREVIOUSLY DECLARED FILE
-//**************************************
-    
-    public ArrayList returnAccounts(){
-        
-        return accountFleet; 
     }
-    
-    
-    
+
+
+    public void saveAccountChanges(){
+        if(accountFleet.size() > 0){
+            try {
+                FileWriter fileWriter = new FileWriter(file);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                for(Account acc : accountFleet){
+                    bufferedWriter.write(acc.getFirstName()+","+
+                            acc.getLastName()+","+
+                            acc.getUserName()+","+
+                            acc.getPassword()+","+
+                            acc.getEmail()+","+
+                            acc.getPhoneNumber());
+                    bufferedWriter.newLine();
+                }
+                bufferedWriter.close();
+            }catch(IOException ex){ ex.printStackTrace(); }
+        }
+    }
+
+
+    public ArrayList returnAccounts(){
+        return accountFleet;
+    }
+
     
     public boolean accountExist(String string){
-       
-        Scanner contains = new Scanner(System.in); 
+
+        Scanner contains = new Scanner(System.in);
         System.out.println("Enter account name.");
-        String contain = contains.next(); 
-       for(int x=0; x< accountFleet.size(); x++){
-           if (accountFleet.contains(contains)){
-               return true;
-           }else{
-               return false;
-              
-           }
-       }
-       return true;
-        
+        String contain = contains.next();
+        for(int x=0; x< accountFleet.size(); x++){
+            if (accountFleet.contains(contains)){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return true;
     }
-    
- public void removeAccount(String string){
-     Scanner remove = new Scanner(System.in); 
+
+    public void removeAccount(String string){
+        Scanner remove = new Scanner(System.in);
         System.out.println("Enter account to remove");
-        String removes = remove.next(); 
-     
+        String removes = remove.next();
+
         Iterator it= accountFleet.iterator();
-       
-     while (it.hasNext()){
-         
-         
-         
-     }
-        
-        
+
+        while (it.hasNext()){
+
+        }
     }
-    
-        
+
+
     public void addAccount(Account account){
-        
-    
-      
+        accountFleet.add(account);
     }
-    
-    
-    
-    
 }
 
 
