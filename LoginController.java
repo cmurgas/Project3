2edc5a3b25a157d36d0c75a62712e615679768d3
CosmicLamb <31945143+CosmicLamb@users.noreply.github.com>
2017-12-04 12:59:34 -0500

    Update LoginController.java
    Office Manager Skeleton Added

--- LoginController.java
+++ LoginController.java
@@ -2,22 +2,20 @@
  * Sample Skeleton for 'Login.fxml' Controller Class
  */
 
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class LoginController {
+    AccountFleet accountFleet;
 
+    //Login in Screen Fields
     @FXML // fx:id="passwordField"
     private TextField passwordField; // Value injected by FXMLLoader
 
@@ -27,13 +25,70 @@ public class LoginController {
     @FXML // fx:id="loginButton"
     private Button loginButton; // Value injected by FXMLLoader
 
+    //Office Manager Screen Fields
+
+    @FXML // fx:id="bikeparts_tab"
+    private AnchorPane bikeparts_tab; // Value injected by FXMLLoader
+
+    @FXML // fx:id="bikeparts_tree"
+    private TreeTableView<?> bikeparts_tree; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_bikename"
+    private TreeTableColumn<?, ?> om_bikename; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_bikenumber"
+    private TreeTableColumn<?, ?> om_bikenumber; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_listPice"
+    private TreeTableColumn<?, ?> om_listPice; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_salesPrice"
+    private TreeTableColumn<?, ?> om_salesPrice; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_onSale"
+    private TreeTableColumn<?, ?> om_onSale; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_quantity"
+    private TreeTableColumn<?, ?> om_quantity; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_search_name"
+    private TextField om_search_name; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_search_number"
+    private TextField om_search_number; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_display_bike"
+    private TextArea om_display_bike; // Value injected by FXMLLoader
+
+    @FXML // fx:id="search"
+    private Button search; // Value injected by FXMLLoader
+
+    @FXML // fx:id="order_parts"
+    private Button order_parts; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_SA_search"
+    private TextField om_SA_search; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_StartDateSearch"
+    private TextField om_StartDateSearch; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_endDateSearch"
+    private TextField om_endDateSearch; // Value injected by FXMLLoader
+
+    @FXML // fx:id="om_displayCommission"
+    private TextArea om_displayCommission; // Value injected by FXMLLoader
+
+    @FXML // fx:id="salesCommissionButton"
+    private Button salesCommissionButton; // Value injected by FXMLLoader
+
+
+    //Login Screen Actions
     @FXML
     void doLogin(ActionEvent event) {
-        AccountFleet accountFleet = new AccountFleet();
-
-        Account account1 = new SystemAdmin("katie","lamb","klamb3","bmal","no","7");
-        accountFleet.addAccount(account1);
+        accountFleet = new AccountFleet();
+        accountFleet.addAccount(new SystemAdmin("katie","lamb","klamb3","bmal","no","7"));
         accountFleet.saveAccountChanges();
+
 
         String userName = userNameField.getText();
         String password = passwordField.getText();
@@ -92,6 +147,17 @@ public class LoginController {
                 //e.printStackTrace();
             }
         }
+
+    }
+
+    //office manager screen actions
+    @FXML
+    void doOrderOM(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSalesCommission(ActionEvent event) {
 
     }
 }
