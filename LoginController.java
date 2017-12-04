86235532c9f681b6b70fbc1cff3255735ca88803
CosmicLamb <31945143+CosmicLamb@users.noreply.github.com>
2017-12-03 22:27:00 -0500

    Update and rename Controller.java to LoginController.java
    I'm still getting a null pointer exception when I try to add account1 to account fleet

---
+++ LoginController.java
@@ -0,0 +1,97 @@
+/**
+ * Sample Skeleton for 'Login.fxml' Controller Class
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController {
+
+    @FXML // fx:id="passwordField"
+    private TextField passwordField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="userNameField"
+    private TextField userNameField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="loginButton"
+    private Button loginButton; // Value injected by FXMLLoader
+
+    @FXML
+    void doLogin(ActionEvent event) {
+        AccountFleet accountFleet = new AccountFleet();
+
+        Account account1 = new SystemAdmin("katie","lamb","klamb3","bmal","no","7");
+        accountFleet.addAccount(account1);
+        accountFleet.saveAccountChanges();
+
+        String userName = userNameField.getText();
+        String password = passwordField.getText();
+
+        Account account = null;
+        Stage stage = null;
+        Parent root = null;
+
+        for (Account a : accountFleet.getAccounts()){
+            if ((a.getUserName().equals(userName) && a.getPassword().equals(password))){
+                account =  a;
+                System.out.println(a.getPassword());
+            }
+        }
+        if (account instanceof SalesAssociate){
+            try {
+                stage = (Stage) loginButton.getScene().getWindow();
+                root = FXMLLoader.load(getClass().getResource("SalesAssociateScreen.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else if (account instanceof WareHouseManager){
+            try {
+                stage = (Stage) loginButton.getScene().getWindow();
+                root = FXMLLoader.load(getClass().getResource("WareHouseManagerScreen.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else if (account instanceof  OfficeManager){
+            try {
+                stage = (Stage) loginButton.getScene().getWindow();
+                root = FXMLLoader.load(getClass().getResource("OfficeManagerScreen.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        else if (account instanceof SystemAdmin){
+            try {
+                stage = (Stage) loginButton.getScene().getWindow();
+                root = FXMLLoader.load(getClass().getResource("SystemAdminScreen.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }catch(Exception e){
+                //e.printStackTrace();
+            }
+        }
+
+    }
+}
